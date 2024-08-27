d0c2a7ae228295d2b806c71027c0fc81db455bd3
veer0246 <veer009292@gmail.com>
2024-08-27 11:21:32 +0530

    create user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,21 @@
-// const mongoose = require('mongoose')
-// const userSchema =  new mongoose.Schema({
-//     name:{
-//         type:String,
-//         minlength:3,
-//         required:true,
-//         trim:true   // it's remove the whitespace from name
-//     },
-//     email:{
-//         type:String,
-//         required:true,
-//         unique:true
-//     },
-//     password:{
-//         type:String,
-//         required:true,
-//     }
+const mongoose = require('mongoose')
+const userSchema =  new mongoose.Schema({
+    name:{
+        type:String,
+        minlength:3,
+        required:true,
+        trim:true   // it's remove the whitespace from name
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }
 
-// },{timestamps:true}) 
+},{timestamps:true}) 
 
-// module.exports = mongoose.model('Users', userSchema)
+module.exports = mongoose.model('Users', userSchema)
